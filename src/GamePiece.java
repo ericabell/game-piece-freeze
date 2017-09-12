b311b190fe6b5076014ce7025521be5951b97ac4
Eric Abell <ericabell@users.noreply.github.com>
2017-09-12 13:17:10 -0500

    More extras complete - board restrictions and move refactored

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -6,10 +6,58 @@ public class GamePiece {
     String name;
     String color;
 
+    int minX;
+    int maxX;
+    int minY;
+    int maxY;
+
     public GamePiece() {
         this.positionX = 0;
         this.positionY = 0;
         this.frozen = false;
+    }
+
+    public GamePiece(int minX, int maxX, int minY, int maxY) {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.frozen = false;
+
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+    }
+
+    public int getMinX() {
+        return minX;
+    }
+
+    public void setMinX(int minX) {
+        this.minX = minX;
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public void setMaxX(int maxX) {
+        this.maxX = maxX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public void setMinY(int minY) {
+        this.minY = minY;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public void setMaxY(int maxY) {
+        this.maxY = maxY;
     }
 
     public int getPositionX() {
@@ -49,11 +97,38 @@ public class GamePiece {
         }
     }
 
-    public void freeze() {
-        this.frozen = true;
+    public void moveWithBoundaries( int newX, int newY ) {
+        if( this.frozen ) {
+            // can't move if we're frozen!
+        } else {
+            // check to see that our destination is within the board
+            if( minX <= newX  && newX <= maxX ) {
+                // ok to move in X
+                this.positionX = newX;
+            }
+            if( minY <= newY && newY <= maxY ) {
+                // ok to move in Y
+                this.positionY = newY;
+            }
+        }
     }
 
-    public void unfreeze() {
+    public boolean freeze() {
+        this.frozen = true;
+        return( this.frozen );
+    }
+
+    public boolean unfreeze() {
         this.frozen = false;
+        return( this.frozen );
+    }
+
+    @Override
+    public String toString() {
+        return "GamePiece{" +
+                "positionX=" + positionX +
+                ", positionY=" + positionY +
+                ", frozen=" + frozen +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,30 +9,82 @@ public class Main {
 
         // move the piece and print the new position using getters
         gp1.move(10,10);
-        System.out.println(gp1.getPositionX() + ", " + gp1.getPositionY());
+        System.out.println(gp1);
 
         // set the color and name of the piece
         gp1.setColor("red");
         gp1.setName("My Chess Piece");
 
         // freeze the piece and verify that it doesn't move
-        gp1.freeze();
-        if( gp1.frozen ) {
+        if( gp1.freeze() ) {
             System.out.println("We froze the piece!");
         }
         gp1.move(20,20);
-        System.out.println(gp1.getPositionX() + ", " + gp1.getPositionY());
+        System.out.println(gp1);
 
         // unfreeze the piece and verify
-        gp1.unfreeze();
-        if( !gp1.frozen ) {
+        if( !gp1.unfreeze() ) {
             System.out.println("We unfroze the piece!");
         }
 
         // verify we can move the piece again now that it isn't frozen
         gp1.move(20,20);
-        System.out.println(gp1.getPositionX() + ", " + gp1.getPositionY());
+        System.out.println(gp1);
 
 
+        // EXTRAS - use Random Ints to move the piece
+        System.out.println("EXTRAS - random position ********** ");
+        Random random1 = new Random();
+        int newX = random1.nextInt(10);
+        int newY = random1.nextInt(10);
+
+        gp1.move(newX, newY);
+        System.out.println(gp1);
+
+        // MORE EXTRAS - set up board restrictions
+        System.out.println("MORE EXTRAS - board restriction *******");
+        GamePiece gp2 = new GamePiece(0,100,0,100);
+
+        System.out.println(gp2);
+
+        // attempt to move the piece outside max X boundary
+        int oldX = gp2.getPositionX();
+        gp2.moveWithBoundaries(101,80);
+        if( gp2.getPositionX() != oldX ) {
+            // something is wrong
+            System.out.println("We shouldn't have moved!");
+        } else {
+            System.out.println("Max X boundary test passed!");
+        }
+
+        // attempt to move the piece outside min X boundary
+        oldX = gp2.getPositionX();
+        gp2.moveWithBoundaries(-1,80);
+        if( gp2.getPositionX() != oldX ) {
+            // something is wrong
+            System.out.println("We shouldn't have moved!");
+        } else {
+            System.out.println("Min X boundary test passed!");
+        }
+        
+        // attempt to move the piece outside Y boundary
+        int oldY = gp2.getPositionY();
+        gp2.moveWithBoundaries(50,101);
+        if( gp2.getPositionY() != oldY ) {
+            // something is wrong
+            System.out.println("We shouldn't have moved!");
+        } else {
+            System.out.println("Max Y boundary test passed!");
+        }
+
+        // attempt to move the piece outside Y boundary
+        oldY = gp2.getPositionY();
+        gp2.moveWithBoundaries(50,-1);
+        if( gp2.getPositionY() != oldY ) {
+            // something is wrong
+            System.out.println("We shouldn't have moved!");
+        } else {
+            System.out.println("Min Y boundary test passed!");
+        }
     }
 }
